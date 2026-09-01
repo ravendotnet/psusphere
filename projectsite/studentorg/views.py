daf7640fcdbaa186380da64fa571b525164abb46
imnotraven <r00vnox@yahoo.com>
2026-09-01 17:12:42 +0800

    added orgmember for deployment

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization
+from studentorg.models import OrgMember
 from studentorg.forms import OrganizationForm 
 from django.urls import reverse_lazy 
 
@@ -33,5 +34,11 @@ class OrganizationDeleteView(DeleteView):
      model = Organization  
      template_name = 'org_del.html' 
      success_url = reverse_lazy('organization-list') 
+
+class OrgMemberList(ListView):
+     model = OrgMember
+     context_object_name = 'orgmember'
+     template_name = 'orgmember_list.html'
+     paginate_by = 5
 
 # Create your views here.
