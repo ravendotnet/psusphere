c05e771c62eb95a730f9596dd8b729cf1cde04ab
imnotraven <r00vnox@yahoo.com>
2026-09-01 22:37:51 +0800

    added crud-update to program models

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -127,4 +127,10 @@ class ProgramCreateView(CreateView):
     form_class = ProgramForm
     template_name = 'program_form.html'
     success_url = reverse_lazy('program-list')
+
+class ProgramUpdateView(UpdateView): 
+    model = Program 
+    form_class = ProgramForm  
+    template_name = 'program_form.html' 
+    success_url = reverse_lazy('program-list') 
 # Create your views here.
