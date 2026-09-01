29112125f0ecdf43a217bde7c313aedf855c2bc0
janepaulineb <balofinosjanepauline@gmail.com>
2026-09-01 14:15:13 +0800

    modify update view

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from studentorg.models import Organization
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy
@@ -22,3 +22,8 @@ class OrganizationList(ListView):
     context_object_name = 'organization'
     template_name = "org_list.html"
     paginate_by = 5
+class OrganizationUpdateView(UpdateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'org_form.html'
+    success_url = reverse_lazy('organization-list')
