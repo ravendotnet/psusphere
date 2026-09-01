8e677c5f125053341bf5a8aa47167ff189d63923
imnotraven <r00vnox@yahoo.com>
2026-09-01 18:49:47 +0800

    added student models to deployment

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 #from studentorg.views import HomePageView
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView, OrgMemberList, OrgMemberCreateView, OrgMemberUpdateView
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView, OrgMemberList, OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView, StudentList
 from studentorg import views
 
 urlpatterns = [
@@ -25,9 +25,11 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('orgmember_list', OrgMemberList.as_view(), name='orgmember-list'),
+    path('student_list', StudentList.as_view(), name = 'student-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
     path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
     path('orgmember_list/add', OrgMemberCreateView.as_view(), name='orgmember-add'),
     path('orgmember_list/<pk>', OrgMemberUpdateView.as_view(), name='orgmember-update'),
+    path('orgmember_list/<pk>/delete', OrgMemberDeleteView.as_view(), name="orgmember-delete"),
 ]
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -3,6 +3,7 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization
 from studentorg.models import OrgMember
+from studentorg.models import Student
 from studentorg.forms import OrgMemberForm
 from studentorg.forms import OrganizationForm 
 from django.urls import reverse_lazy 
@@ -18,6 +19,12 @@ class OrganizationList(ListView):
     context_object_name = 'organization'
     template_name = "org_list.html"
     paginate_by = 5
+
+class StudentList(ListView):
+     model = Student
+     context_object_name = 'student'
+     template_name = 'student_list.html'
+     paginate_by = 5
 
 class OrganizationCreateView(CreateView): 
      model = Organization 
@@ -53,6 +60,11 @@ class OrgMemberUpdateView(UpdateView):
      model = OrgMember 
      form_class = OrgMemberForm  
      template_name = 'orgmember_form.html' 
+     success_url = reverse_lazy('orgmember-list') 
+
+class OrgMemberDeleteView(DeleteView): 
+     model = OrgMember  
+     template_name = 'orgmember_del.html' 
      success_url = reverse_lazy('orgmember-list') 
 
     
