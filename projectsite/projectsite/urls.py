aee3ff847329ddcc5c8a4f50b66f5af665b7cf20
janepaulineb <balofinosjanepauline@gmail.com>
2026-09-09 12:41:12 +0800

    add allauth urls

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from studentorg.views import HomePageView
 from studentorg.views import OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 from studentorg.views import Org_MemberList, Org_MemberCreateView, Org_MemberUpdateView, Org_MemberDeleteView
@@ -26,6 +26,7 @@ from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/",include("allauth.urls")), #allauth routes
     path('', views.HomePageView.as_view(), name='home'),
 
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
